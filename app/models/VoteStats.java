26aef3ecf059b536a6ec5d40cb426df0bbab2f4a
Fjen Undso <fjen@neboola.de>
2015-06-23 20:34:13 +0200

    models/VoteStats: add currentusers type and cleanup

--- app/models/VoteStats.java
+++ app/models/VoteStats.java
@@ -2,49 +2,25 @@ package models;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-import play.data.format.Formats;
-import play.data.validation.Constraints;
 import play.db.ebean.Model;
 
-import com.avaje.ebean.annotation.EnumValue;
-
-@Entity
 public class VoteStats extends Model {
 
-	private static final long serialVersionUID = 8660259739658830119L;
+	private static final long serialVersionUID = -4667216362514477494L;
 
-	@Id
-	public Long id;
-
-	@Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
 	public Date date = new Date();
 
-	@Column(nullable = true)
 	public Integer value;
 
-	@Constraints.Required
 	public Type type;
 
 	public enum Type {
-		@EnumValue("A")
 		ALL,
-		
-		@EnumValue("S")
 		SPEED,
-
-		@EnumValue("U")
 		UNDERSTANDABILITY,
-
-		@EnumValue("R")
 		REQUEST,
+		CURRENTUSERS,
 	}
-
-	public static Finder<Long, VoteStats> find = new Finder<Long, VoteStats>(Long.class,
-			VoteStats.class);
 
 	public VoteStats(Type type, Integer value) {
 		this.type = type;
